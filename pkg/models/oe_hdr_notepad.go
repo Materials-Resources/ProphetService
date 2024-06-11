533466bdc19093f211f849cf192bb5d8c3d66e44
Collin Smallegan <cmsmallegan@comsma.com>
2024-06-11 16:47:28 -0400

    removed pointers

--- pkg/models/oe_hdr_notepad.go
+++ pkg/models/oe_hdr_notepad.go
@@ -8,10 +8,10 @@ import (
 type OeHdrNotepad struct {
 	bun.BaseModel    `bun:"table:oe_hdr_notepad"`
 	NoteId           float64   `bun:"note_id,type:decimal(19,0),pk"`
-	OrderNo          *string   `bun:"order_no,type:varchar(8),nullzero"`
-	NotepadClassId   *string   `bun:"notepad_class_id,type:varchar(8),nullzero"`
+	OrderNo          string    `bun:"order_no,type:varchar(8),nullzero"`
+	NotepadClassId   string    `bun:"notepad_class_id,type:varchar(8),nullzero"`
 	Topic            string    `bun:"topic,type:varchar(30)"`
-	Note             *string   `bun:"note,type:text(2147483647),nullzero"`
+	Note             string    `bun:"note,type:text(2147483647),nullzero"`
 	ActivationDate   time.Time `bun:"activation_date,type:datetime"`
 	ExpirationDate   time.Time `bun:"expiration_date,type:datetime"`
 	EntryDate        time.Time `bun:"entry_date,type:datetime"`
@@ -21,6 +21,6 @@ type OeHdrNotepad struct {
 	DateLastModified time.Time `bun:"date_last_modified,type:datetime"`
 	LastMaintainedBy string    `bun:"last_maintained_by,type:varchar(30),default:(user_name(null))"`
 	DefaultInOe      string    `bun:"default_in_oe,type:char"`
-	GeneralNote      *string   `bun:"general_note,type:char,nullzero"`
+	GeneralNote      string    `bun:"general_note,type:char,nullzero"`
 	CreatedBy        string    `bun:"created_by,type:varchar(255),default:(suser_sname())"`
 }
