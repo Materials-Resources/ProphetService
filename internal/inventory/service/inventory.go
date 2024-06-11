e5987e6820f90c0b57c3f2b89c1e455f39c67815
Collin Smallegan <cmsmallegan@comsma.com>
2024-06-11 12:22:48 -0400

    added models for
    - oe_line_last_margin_price
    - oe_line_promise_date
    - oe_line_tax

--- internal/inventory/service/inventory.go
+++ internal/inventory/service/inventory.go
@@ -2,35 +2,34 @@ package service
 
 import (
 	"context"
-	"fmt"
-	"github.com/materials-resources/s-prophet/infrastructure/data"
 	"github.com/materials-resources/s-prophet/internal/inventory/domain"
+	"github.com/materials-resources/s-prophet/pkg/models"
 )
 
 type InventoryService struct {
-	models data.Models
+	models models.Models
 }
 
 func (s *InventoryService) GetProductStock(ctx context.Context, int32s []int32) ([]domain.ProductStock, error) {
-	invLocs, err := s.models.InvLoc.GetByInvMastUids(ctx, int32s)
-	if err != nil {
-		return nil, err
-
-	}
-	productStocks := make([]domain.ProductStock, len(invLocs))
-
-	for i, invLoc := range invLocs {
-		productStocks[i] = domain.ProductStock{
-			ProductUid:        invLoc.InvMastUid,
-			QuantityAvailable: invLoc.QtyOnHand.Float64,
-		}
-
-	}
+	//invLocs, err := s.models.InvLoc.GetByInvMastUids(ctx, int32s)
+	//if err != nil {
+	//	return nil, err
+	//
+	//}
+	//productStocks := make([]domain.ProductStock, len(invLocs))
+	//
+	//for i, invLoc := range invLocs {
+	//	productStocks[i] = domain.ProductStock{
+	//		ProductUid:        invLoc.InvMastUid,
+	//		QuantityAvailable: invLoc.QtyOnHand.Float64,
+	//	}
+	//
+	//}
 	// TODO implement me
 	panic("implement me")
 }
 
-func NewInventoryService(models data.Models) *InventoryService {
+func NewInventoryService(models models.Models) *InventoryService {
 	return &InventoryService{models: models}
 }
 
@@ -46,7 +45,7 @@ func (s *InventoryService) GetInventoryReceiptById(ctx context.Context, id float
 		return domain.InventoryReceipt{}, err
 	}
 
-	invTransMap := make(map[int32][]*data.InvTran)
+	invTransMap := make(map[int32][]*models.InvTran)
 	for _, invTran := range invTrans {
 		invTransMap[invTran.InvMastUid] = append(invTransMap[invTran.InvMastUid], invTran)
 	}
@@ -56,35 +55,35 @@ func (s *InventoryService) GetInventoryReceiptById(ctx context.Context, id float
 		invMastUids[i] = line.InvMastUid
 	}
 
-	invLocs, err := s.models.InvLoc.GetByInvMastUids(ctx, invMastUids)
-	if err != nil {
-		return domain.InventoryReceipt{}, err
-	}
-	invLocsMap := make(map[int32]*data.InvLoc)
-	for _, invLoc := range invLocs {
-		invLocsMap[invLoc.InvMastUid] = invLoc
-	}
-
-	items := make([]domain.InventoryReceiptItem, len(inventoryReceiptsHdr.InventoryReceiptsLines))
-	for i, line := range inventoryReceiptsHdr.InventoryReceiptsLines {
-
-		allocatedOrders := make([]string, len(invTransMap[line.InvMastUid]))
-
-		for j, tran := range invTransMap[line.InvMastUid] {
-			allocatedOrders[j] = fmt.Sprintf("%.0f", tran.DocumentNo)
-		}
-
-		items[i] = domain.InventoryReceiptItem{
-			ProductUid:       line.InvMastUid,
-			ReceivedQuantity: line.QtyReceived,
-			ReceivedUnit:     line.UnitOfMeasure,
-			AllocatedOrders:  allocatedOrders,
-			PrimaryBin:       invLocsMap[line.InvMastUid].PrimaryBin.String,
-		}
-	}
+	//invLocs, err := s.models.InvLoc.GetByInvMastUids(ctx, invMastUids)
+	//if err != nil {
+	//	return domain.InventoryReceipt{}, err
+	//}
+	//invLocsMap := make(map[int32]*data.InvLoc)
+	//for _, invLoc := range invLocs {
+	//	invLocsMap[invLoc.InvMastUid] = invLoc
+	//}
+	//
+	//items := make([]domain.InventoryReceiptItem, len(inventoryReceiptsHdr.InventoryReceiptsLines))
+	//for i, line := range inventoryReceiptsHdr.InventoryReceiptsLines {
+	//
+	//	allocatedOrders := make([]string, len(invTransMap[line.InvMastUid]))
+	//
+	//	for j, tran := range invTransMap[line.InvMastUid] {
+	//		allocatedOrders[j] = fmt.Sprintf("%.0f", tran.DocumentNo)
+	//	}
+	//
+	//	items[i] = domain.InventoryReceiptItem{
+	//		ProductUid:       line.InvMastUid,
+	//		ReceivedQuantity: line.QtyReceived,
+	//		ReceivedUnit:     line.UnitOfMeasure,
+	//		AllocatedOrders:  allocatedOrders,
+	//		PrimaryBin:       invLocsMap[line.InvMastUid].PrimaryBin.String,
+	//	}
+	//}
 	return domain.InventoryReceipt{
 		ID:    inventoryReceiptsHdr.ReceiptNumber,
-		Items: items,
+		Items: nil,
 	}, nil
 
 }
---
+++ pkg/models/oe_line_promise_date.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"database/sql"
+	"github.com/uptrace/bun"
+	"time"
+)
+
+type OeLinePromiseDate struct {
+	bun.BaseModel                   `bun:"table:oe_line_promise_date"`
+	OeLineUid                       int32          `bun:"oe_line_uid,type:int"`
+	OriginalPromiseDate             sql.NullTime   `bun:"original_promise_date,type:datetime,nullzero"`
+	PromiseDate                     time.Time      `bun:"promise_date,type:datetime"`
+	EditedFlag                      string         `bun:"edited_flag,type:char"`
+	AroDays                         sql.NullInt32  `bun:"aro_days,type:int,nullzero"`
+	DateCreated                     time.Time      `bun:"date_created,type:datetime,default:(getdate())"`
+	CreatedBy                       string         `bun:"created_by,type:varchar(255),default:(suser_sname())"`
+	DateLastModified                time.Time      `bun:"date_last_modified,type:datetime,default:(getdate())"`
+	LastMaintainedBy                string         `bun:"last_maintained_by,type:varchar(255),default:(suser_sname())"`
+	OeLinePromiseDateUid            int32          `bun:"oe_line_promise_date_uid,type:int,pk"`
+	PromiseDateEditedDate           sql.NullTime   `bun:"promise_date_edited_date,type:datetime,nullzero"`
+	ExtendedDesc                    sql.NullString `bun:"extended_desc,type:varchar(8000),nullzero"`
+	Alert                           sql.NullString `bun:"alert,type:varchar,nullzero"`
+	PreviousPromiseDate             sql.NullTime   `bun:"previous_promise_date,type:datetime,nullzero"`
+	InsufficientDataToCalculateFlag sql.NullString `bun:"insufficient_data_to_calculate_flag,type:char,nullzero"`
+}
